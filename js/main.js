7d56675008bb988b2f483c6c1728c76e33ccf7dc
Claudio Campos <claudio@claudiocampos.com>
2013-01-05 22:29:17 -0500

    added requireJS and changed structure accordingly

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// Require.js allows us to configure shortcut aliases
+// Their usage will become more apparent further along in the tutorial.
+require.config({
+  paths: {
+    jquery: 'libs/jquery/jquery',
+    underscore: 'libs/underscore/underscore',
+    backbone: 'libs/backbone/backbone'
+  }
+});
+
+require([
+  // Load our app module and pass it to our definition function
+  'app',
+], function(App){
+  // The "app" dependency is passed in as "App"
+  App.initialize();
+});
